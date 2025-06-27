9101f485ccfd34401ae90d1872c7cc2ac1586ca8
Izzy Mones <izzymones@Izzys-MacBook-Pro-2.local>
2025-06-27 13:04:54 -0700

    adding cascaded simulator

--- simulator.py
+++ simulator.py
@@ -212,3 +212,77 @@ class KalmanFilterTuner:
             plt.legend(loc='lower right')
             plt.show()
 
+class CascadedSimulator:
+    def __init__(self, model, x0, u0, timespan):
+        self.model = model
+        self.x0 = x0
+        self.u0 = u0
+        self.dt = self.model.dt
+        self.tspan = np.arange(0,timespan, self.dt)
+        self.data = np.empty([len(self.tspan),self.model.state_size() + self.model.input_size()])
+        self.input_bound = np.array([])
+
+    def add_intput_bound(self, bound):
+        assert len(bound) == len(self.u0)
+        self.input_bound = bound
+
+    def run(self):
+        x = self.x0
+        u = self.u0
+
+        # if our model doesn't have a Kalman Filter then y
+        # will just be a dummy variable
+        y = self.x0
+        if self.model.has_kalman_filter():
+            y = self.model.C@self.x0 
+
+        start_time = perf_counter()
+        for i in range(len(self.tspan)):
+            x = self.model.next_state(x,u,y)
+            u = self.model.control_input(x)
+
+            # if we have a Kalman Filter then just set
+            # the sensor values to the state
+            if self.model.has_kalman_filter():
+                y = self.model.C@x
+
+            self.data[i] =  np.append(x, u)
+        print('Time for {} iterations of {} is {}'.format(len(self.tspan), self.model.model_name(), perf_counter() - start_time))
+
+
+        plt.rcParams.update({'font.size': 12})
+        plt.rcParams.update({
+        "text.usetex": True,
+        })
+
+        ns = self.model.state_size()
+
+        for i in range(ns):
+            plt.plot(self.tspan,self.data[:,i],linewidth=2,label=self.model.state_names()[i])
+        plt.xlabel('Time')
+        plt.ylabel('State')
+        plt.title(self.model.model_name())
+        plt.legend(loc='lower right')
+        plt.show()
+
+
+        nu = self.model.input_size()
+        fig, axs = plt.subplots(ns + nu)
+        fig.set_figheight(8)
+        fig.suptitle(self.model.model_name())
+        for i in range(ns):
+            axs[i].plot(self.tspan,self.data[:,i],linewidth=2)
+            axs[i].set_ylabel(self.model.state_names()[i])
+        for i in range(nu):
+            axs[i+ns].plot(self.tspan,self.data[:,i+ns],linewidth=2)
+            #axs[i+ns].set_ylabel(self.model.u[i].name())
+
+            if self.input_bound.any():
+                if self.input_bound[i].any():
+                    upper = np.full((len(self.tspan),), self.input_bound[i][0])
+                    lower = np.full((len(self.tspan),), self.input_bound[i][1])
+                    axs[i+ns].plot(self.tspan,upper,linewidth=1)
+                    axs[i+ns].plot(self.tspan,lower,linewidth=1)
+                
+        plt.xlabel('Time')
+        plt.show()
