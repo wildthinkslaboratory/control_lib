44db2edeb0216468818681875f5fa59e082f9353
wildthinkslaboratory <goldfishandrobin@gmail.com>
2025-06-26 18:17:45 -0700

    adding some example files and data

---
+++ example2.py
@@ -0,0 +1,151 @@
+import casadi as ca
+from casadi import sin, cos
+from model import LQRModel, LQGModel, LQRDModel, LQGDModel
+from simulator import Simulator, NoisySimulator, KalmanFilterTuner
+import numpy as np
+from utilities import import_data
+
+
+# We create Casadi symbolic variables for the state
+theta = ca.MX.sym('theta')
+thetadot = ca.MX.sym('thetadot')
+u = ca.MX.sym('u')             # control input
+state = ca.vertcat(
+    theta,
+    thetadot,
+)
+
+# create casadi constants
+M = ca.MX.sym('M') 
+m = ca.MX.sym('m') 
+L = ca.MX.sym('L') 
+g = ca.MX.sym('g') 
+d = ca.MX.sym('d')
+
+
+constants = ca.vertcat( M, m, L, g, d )
+
+# we build the nonlinear function f for the equations of motion
+# we begin with some partial expressions to make the formulas easier to build
+
+
+denominator = M + m*(sin(theta)**2)
+n0 = -m*g*sin(theta)*cos(theta)
+n1 = m*L*(sin(theta))*(thetadot)**2 + u
+n2 = (m + M)*g*sin(theta)
+RHS = ca.vertcat( 
+    thetadot, 
+    (n2+(-cos(theta))*(n1)) / (L*denominator)
+    )
+
+# the constant values are imported from the build file
+M_ = 0.05       # wheels plus motors (kilograms) 
+m_ = 1          # rest of the robot (kilograms)
+L_ = 1.2        # length of pendulum (meters)
+g_ = -9.81       # gravity, (meters / sec^2)
+d_ = 0.01       # d is a damping factor
+
+constant_values = [M_, m_, L_, g_, d_]
+
+# I made latex names for my states. They look nice in the simulation plots
+my_state_names = ['$\\theta$ ','$\\dot{\\theta}$ ']
+
+dt = 0.01
+# Now we make our model.
+lqrBot = LQRModel(state, 
+                RHS, 
+                u, 
+                constants, 
+                constant_values, 
+                dt,
+                state_names=my_state_names,
+                name='balancing robot LQR')
+
+# set up the K matrix
+Q = np.eye(2)
+R = np.eye(1)
+goal_state = [np.pi, 0.0]
+goal_u = [0.0]
+
+lqrBot.set_up_K(Q, R, goal_state, goal_u)
+
+lqgBot = LQGModel(state, 
+                RHS, 
+                u, 
+                constants, 
+                constant_values, 
+                dt,
+                state_names=my_state_names,
+                name='balancing robot LQG')
+
+lqgBot.set_up_K(Q, R, goal_state, goal_u)
+
+# If we want a Kalman Filter we need to pass in a measurement model
+C = np.array([[1, 0], \
+            [0, 1]]) 
+
+V_d = np.eye(2)
+V_n = np.eye(2)
+# V_n = np.diag([0.0001, 0.037, 0.0000029]) # computed variance for MPU6050
+
+lqgBot.set_up_kalman_filter(C, V_d, V_n)
+
+lqrdBot = LQRDModel(state, 
+                RHS, 
+                u, 
+                constants, 
+                constant_values, 
+                dt,
+                state_names=my_state_names,
+                name='balancing robot LQRD')
+
+lqrdBot.set_up_K(Q, R, goal_state, goal_u)
+
+
+lqgdBot = LQGDModel(state, 
+                RHS, 
+                u, 
+                constants, 
+                constant_values, 
+                dt,
+                state_names=my_state_names,
+                name='balancing robot LQGD')
+
+
+lqgdBot.set_up_K(Q, R, goal_state, goal_u)
+lqgdBot.set_up_kalman_filter(C, V_d, V_n)
+
+
+if __name__ == "__main__":
+    # now we can rum a simulation
+    u0 = np.array([0.0])
+    x0 = np.array([np.pi + 0.3, 0.0]) # Initial condition
+    sim_length = 4 # in seconds
+
+    # simulator = Simulator(lqrBot, x0, u0, sim_length)
+    # simulator.run()
+
+    # simulator = Simulator(lqgBot, x0, u0, sim_length)
+    # simulator.run()
+
+    # simulator = Simulator(lqrdBot, x0, u0, sim_length)
+    # simulator.run()
+
+    # simulator = Simulator(lqgdBot, x0, u0, sim_length)
+    # simulator.run()
+
+    # simulator = NoisySimulator(lqgBot, x0, u0, sim_length)
+    # simulator.run()
+
+    # simulator = NoisySimulator(lqgdBot, x0, u0, sim_length)
+    # simulator.run()
+
+    run_data = import_data('data.json')
+    num_cols = len(run_data[0])
+    sensor_data = []
+    for col in range(num_cols):
+        A = [d[col] for d in run_data]
+        sensor_data.append(A)
+
+    filter_tuner = KalmanFilterTuner(lqgdBot, sensor_data)
+    filter_tuner.run()
